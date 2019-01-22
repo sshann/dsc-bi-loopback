1bd48f3c8319b26dcfb9cfeb19dda270786253d5
Wesley Shann <sshann95@outlook.com>
2019-01-22 15:15:01 +0100

    Set ACL permissions
    Not completed.

--- common/models/company.js
+++ common/models/company.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Company) {
 
+  // This ACL rule is throwing a http error 429 to the company resource.
+
+  // {
+  //   "accessType": "*",
+  //   "principalType": "ROLE",
+  //   "principalId": "business_manager",
+  //   "permission": "DENY"
+  // }
+
 };

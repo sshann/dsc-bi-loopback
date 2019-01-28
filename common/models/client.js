33d0605d26beec804915aee30ebd2d32d913f3f6
Wesley Shann <sshann95@outlook.com>
2019-01-28 22:06:25 +0100

    Split getUserRoles function requests to database in timeouts to avoit Too Many Requests error

--- common/models/client.js
+++ common/models/client.js
@@ -4,31 +4,44 @@ var _ = require('underscore');
 
 module.exports = function(Client) {
   // getRolesById was found in the gist https://gist.github.com/zhenyanghua/d24ea57cd70e69bcb82dc3bc8f14ea74
+  // Timeouts are added due the quota limit in the database
   Client.getRolesById = function(id, cb) {
     Client.getApp(function(err, app) {
       if (err) throw err;
       var RoleMapping = app.models.RoleMapping;
       var Role = app.models.Role;
-      RoleMapping.find({where: {principalId: id}}, function(err, roleMappings) {
-        if (!roleMappings.length) {
-          return cb(null, {'roles': []});
-        }
-        var roleIds = _.uniq(roleMappings
-          .map(function(roleMapping) {
-            return roleMapping.roleId;
-          }));
-        var conditions = roleIds.map(function(roleId) {
-          return {id: roleId};
-        });
+      var conditions;
 
-        Role.find({where: {or: conditions}}, function(err, roles) {
+      setTimeout(getRoleMapping, 1000);
+
+      function getRoleMapping() {
+        const RMfilter = {where: {principalId: id}};
+        RoleMapping.find(RMfilter, function(err, roleMappings) {
+          if (!roleMappings.length) {
+            return cb(null, {'roles': []});
+          }
+          var roleIds = _.uniq(roleMappings
+            .map(function(roleMapping) {
+              return roleMapping.roleId;
+            }));
+          conditions = roleIds.map(function(roleId) {
+            return {id: roleId};
+          });
+
+          setTimeout(getRoles, 1000);
+        });
+      }
+
+      function getRoles() {
+        const roleFilter = {where: {or: conditions}};
+        Role.find(roleFilter, function(err, roles) {
           if (err) throw err;
           var roleNames = roles.map(function(role) {
             return role.name;
           });
           cb(null, {'roles': roleNames});
         });
-      });
+      }
     });
   };
   Client.remoteMethod('getRolesById', {
